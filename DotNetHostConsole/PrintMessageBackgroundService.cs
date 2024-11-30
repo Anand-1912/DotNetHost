6160c8e9344c6cd46b57ae62d5d77500cfd8d115
anandha-chandrasekaran-bzy <92104053+anandha-chandrasekaran-bzy@users.noreply.github.com>
2024-11-30 21:24:57 +0000

    Add project files.

---
+++ DotNetHostConsole/PrintMessageBackgroundService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetHostConsole;
+
+internal class PrintMessageBackgroundService : BackgroundService
+{
+    private readonly ILogger<PrintMessageBackgroundService> _logger;
+    private readonly IConfiguration _config;
+    public PrintMessageBackgroundService(ILogger<PrintMessageBackgroundService> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        string message = _config.GetValue<string>(nameof(message)) ?? ""; 
+
+        while(!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation(message);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
---
+++ DotNetHostConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using DotNetHostConsole;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var appBuilder = Host.CreateApplicationBuilder(args); //configures the host with defaults
+
+appBuilder.Services.AddHostedService<PrintMessageBackgroundService>();
+
+var app = appBuilder.Build(); // creates the app
+
+app.Run();
